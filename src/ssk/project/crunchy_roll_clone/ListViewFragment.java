14f776e58dc7d9db2c7d26405e9faa9064318d48
Sophal Kim <sophal.kim.work@gmail.com>
2014-09-16 15:54:02 -0700

    Interesting, customAdapter booted up with no images or text

--- src/ssk/project/crunchy_roll_clone/ListViewFragment.java
+++ src/ssk/project/crunchy_roll_clone/ListViewFragment.java
@@ -11,12 +11,22 @@ import android.widget.ListView;
 public class ListViewFragment extends Fragment {
 
 	String[] items = {"Anime", "Drama", "Search", "Settings", "Help", "Login"};
+	Integer[] images = {
+			R.drawable.dragon_symbol,
+			R.drawable.dragon_symbol,
+			R.drawable.ab_solid_example,
+			R.drawable.dragon_symbol,
+			R.drawable.ab_background_textured_example,
+			R.drawable.dragon_symbol,
+			
+	};
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.listview_layout_fragment, null);
 		ListView lv = (ListView) v.findViewById(R.id.listview);
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, items);
+		CustomArrayAdapter adapter = new CustomArrayAdapter(getActivity(), items, images);
+//		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, items);
 		lv.setAdapter(adapter);
 		return v;
 	}
