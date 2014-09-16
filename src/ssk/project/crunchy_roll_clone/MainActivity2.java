8ca446812ab171411a00f4c7767c96acad4a8b6e
Sophal Kim <sophal.kim.work@gmail.com>
2014-09-16 14:10:52 -0700

    Adapter and Fragment Added

--- src/ssk/project/crunchy_roll_clone/MainActivity2.java
+++ src/ssk/project/crunchy_roll_clone/MainActivity2.java
@@ -2,13 +2,17 @@ package ssk.project.crunchy_roll_clone;
 
 import java.util.Locale;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBar.Tab;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class MainActivity2 extends ActionBarActivity implements ActionBar.TabListener {
 
@@ -48,8 +52,34 @@ public class MainActivity2 extends ActionBarActivity implements ActionBar.TabLis
 		@Override
 		public CharSequence getPageTitle(int position) {
 			Locale l = Locale.getDefault();
-			
+			switch (position) {
+				case 0: return getString(R.string.title_section1).toUpperCase(l);
+				case 1: return getString(R.string.title_section2).toUpperCase(l);
+			}
+			return null;
 		}
 	}
-
+	
+	public static class PlaceholderFragment extends Fragment {
+		
+		private static final String ARG_SECTION_NUMBER = "section_number";
+		
+		public static PlaceholderFragment newInstance(int sectionNumber) {
+			PlaceholderFragment fragment = new PlaceholderFragment();
+			Bundle args = new Bundle();
+			args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+			fragment.setArguments(args);
+			return fragment;
+		}
+		
+		public PlaceholderFragment() {
+		}
+		
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+			return rootView;
+		}
+	}
+ 
 }
